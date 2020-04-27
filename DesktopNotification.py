64b2a247a93f83d9f003905e135731f160528556
Ashish138200 <53967293+Ashish138200@users.noreply.github.com>
2020-04-27 20:04:25 +0530

    Desktop Notification

--- DesktopNotification.py
+++ DesktopNotification.py
@@ -1,3 +1,5 @@
+from tkinter import *
+
 import requests
 from win10toast import ToastNotifier
 import datetime
@@ -10,7 +12,7 @@ except:
 if data is not None:
     getData = data.json()
     covid_India = getData["cases_time_series"]                      # national level data
-    title = """Covid-19 India / {}""".format(datetime.date.today())
+    title = """Ashish's COVID-19 India Live  {}""".format(datetime.date.today())
     n = len(covid_India)
     a = covid_India[n-1]                                            # current date data
     l = list(a.values())
@@ -21,10 +23,10 @@ if data is not None:
     tc = l[4]                                                       # total confirmed
     td = l[5]                                                       # total deaths
     tr = l[6]                                                       # total recovered
-    m1 =  "In India covid-19 total cases is: %s " %tc
-    m2 = "Total Deaths: %s " %td
-    m3 = "Total Recovered: %s " %tr
-    m4= "Today's confirmed cases: %s"%dconf
+    m1 = "Total cases is: %s " %tc +"\n"
+    m2 = "Total Deaths: %s " %td  + "\n"
+    m3 = "Total Recovered: %s " %tr + "\n"
+    m4=  "Today's confirmed cases: %s"%dconf + "\n"
     message = m1+m2+m3+m4
     toaster = ToastNotifier()
     toaster.show_toast(title, message, icon_path="Corona.ico", duration=100)
